20caf60d3df52810d6060dbdff2e1a6a4b1241de
Tricia Swift <tricia.swift8@gmail.com>
2023-10-20 14:01:30 -0500

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import { Route, Routes } from "react-router-dom";
+import { Outlet, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { ShippingList } from "./components/ShippingList";
+import { NavBar } from "./components/navbar/NavBar";
 
 function App() {
   return (
     <Routes>
-      <Route path="/">
+      <Route
+        path="/"
+        element={
+          <>
+            <NavBar />
+            <Outlet />
+          </>
+        }
+      >
         <Route index element={<ShippingList />} />
       </Route>
     </Routes>
