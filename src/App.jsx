19064993b459a0c8d9e85eb059f95c7267b38f36
Tricia Swift <tricia.swift8@gmail.com>
2023-10-23 17:09:09 -0500

    PUT

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { Outlet, Route, Routes } from "react-router-dom";
-import { ShippingList } from "./components/ShippingList";
+import { AllList } from "./components/shippingShips/AllList";
 import { NavBar } from "./components/navbar/NavBar";
-import { ShipList } from "./components/ships/ShipList";
-import { HaulerList } from "../haulers/HaulerList";
-import { EditHauler } from "../forms/EditHauler";
+import { ShipList } from "./components/shippingShips/ShipList";
+import { HaulerList } from "./components/shippingShips/HaulerList";
+import { EditHauler } from "./components/forms/EditHauler";
+import { DockList } from "./components/shippingShips/DockList";
 
 function App() {
   return (
@@ -17,10 +18,11 @@ function App() {
           </>
         }
       >
-        <Route index element={<ShippingList />} />
+        <Route index element={<AllList />} />
         <Route path="shippingships" element={<ShipList />} />
         <Route path="haulingships" element={<HaulerList />} />
         <Route path="haulingships/:haulerId" element={<EditHauler />} />
+        <Route path="docks" element={<DockList />} />
       </Route>
     </Routes>
   );
--- src/services/hualerService.jsx
+++ src/services/hualerService.jsx
@@ -1,3 +1,19 @@
 export const getAllHaulers = () => {
   return fetch(`http://localhost:8000/haulers`).then((res) => res.json());
 };
+
+export const getHaulerById = (haulerId) => {
+  return fetch(`http://localhost:8000/haulers/${haulerId}`).then((res) =>
+    res.json()
+  );
+};
+
+export const updateHauler = (haulerObj) => {
+  return fetch(`http://localhost:8000/haulers/${haulerObj.id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(haulerObj),
+  });
+};
